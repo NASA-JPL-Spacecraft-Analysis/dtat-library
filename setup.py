f5db832a01afad0d981e3b716c803a1a1fb90c4e
Matthew R Muszynski <muszynsk@srl-wsts-ms-1.jpl.nasa.gov>
2025-05-12 20:21:58 +0000

    swaps around setup management to be more like other TTS tools

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
-"""Basic setup for the application"""
-
-from setuptools import setup
-
-setup()
+#Helper file to support installing locally on older Python systems
+#once we can move 100% to a system like pyproject.toml or poetry
+#we can ditch this file and setup_from_pyproject. But as long
+#as we have missions on 3.6.4, we should keep it in place so
+#we can dev locally on those projects and install this locally
+from tts_deployments.setup_from_pyproject import setup_from_pyproject
+setup_from_pyproject()
